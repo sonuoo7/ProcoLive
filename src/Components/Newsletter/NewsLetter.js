6b6fe0f7c9821df9fc8059331b5f3d2eb646b18f
Sonu Pant007 <sonupant007@gmail.com>
2023-05-14 16:28:29 +0530

    update subscribe

--- src/Components/Newsletter/NewsLetter.js
+++ src/Components/Newsletter/NewsLetter.js
@@ -9,7 +9,7 @@ const NewsLetter = () => {
       <div className="newsletter">
         <Row>
           <Col lg="12" className="text-center">
-            <h2 className="mb-4">Subscribe Our Newsletter</h2>
+            <h2 className="mb-4">Subscribe</h2>
             <div className="subscribe">
               <input type="text" placeholder="Email" />
               <button className="btn align-content-center">Subscribe</button>
